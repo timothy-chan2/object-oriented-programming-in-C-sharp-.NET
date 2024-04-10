8374e9d8e37965e6e74a6d96b20e10d1d31494bf
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-04-09 23:40:52 -0400

    Practice using variables to do simple addition

---
+++ Fundamentals/BasicMath/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte firstNumber = 1;
+            sbyte secondNumber = 2;
+            sbyte thirdNumber = 3;
+            sbyte answer = (sbyte)(firstNumber + secondNumber + thirdNumber);
+
+            Console.WriteLine(firstNumber + " + " + secondNumber + " + " + thirdNumber + " = " + answer);
+            Console.ReadLine();
+        }
+    }
+}
