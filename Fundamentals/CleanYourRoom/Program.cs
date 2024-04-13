299a22dfec2b5d380717dcd8e13a336bd95226a8
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-04-13 00:21:57 -0400

    Create a program that uses a do-while loop and a random number generator so it will loop an unknown number of times

---
+++ Fundamentals/CleanYourRoom/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CleanYourRoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timesRepeated = 1;
+            double chanceOfCleaningTheRoom = 0.0;
+            double increaseInChanceOfCleaning;
+            Random numberGenerator = new Random();
+
+            do
+            {
+                Console.WriteLine($"Request to clean your room is expressed! (x{timesRepeated})");
+                timesRepeated++;
+
+                increaseInChanceOfCleaning = numberGenerator.NextDouble() * ((25 - 10) + 10) / 100;
+                chanceOfCleaningTheRoom += increaseInChanceOfCleaning;
+            } while (chanceOfCleaningTheRoom < 1.0 && timesRepeated < 9);
+
+            Console.WriteLine("Acknowledged. I will proceed with cleaning my room.");
+            Console.ReadLine();
+        }
+    }
+}
