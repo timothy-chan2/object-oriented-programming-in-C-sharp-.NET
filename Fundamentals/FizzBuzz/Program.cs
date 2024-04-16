812450d798100fce54a7389356abe6be1c2b77ba
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-04-16 00:32:53 -0400

    Create a program that shows the user the number of fizz and buzz that they want to see

---
+++ Fundamentals/FizzBuzz/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentCount = 0;
+            int countGoal;
+
+            Console.Write("How many fizz and buzz do you want to see? ");
+            string countGoalAsString = Console.ReadLine();
+
+            if (int.TryParse(countGoalAsString, out countGoal))
+            {
+                int maxRange = countGoal * 5;
+
+                for (int i = 0; i < maxRange; i++)
+                {
+                    if (i == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    else
+                    {
+                        if (i % 3 == 0 && i % 5 == 0)
+                        {
+                            Console.WriteLine("fizzbuzz");
+                            currentCount++;
+                        }
+                        else if (i % 3 == 0)
+                        {
+                            Console.WriteLine("fizz");
+                            currentCount++;
+                        }
+                        else if (i % 5 == 0)
+                        {
+                            Console.WriteLine("buzz");
+                            currentCount++;
+                        }
+                        else
+                        {
+                            Console.WriteLine(i);
+                        }
+
+                        if (currentCount == countGoal)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("You entered an invalid input. You must enter a number.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
