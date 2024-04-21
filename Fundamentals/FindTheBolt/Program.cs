5130f1bab10124578c58870c926f8b7d667a42d2
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-04-21 14:17:43 -0400

    Create a program that finds the index of the word Bolt

---
+++ Fundamentals/FindTheBolt/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FindTheBolt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] hidingSpots = new string[100];
+            int numberOfHidingSpots = hidingSpots.Length;
+
+            Random numberGenerator = new Random();
+            int randomSpot = numberGenerator.Next(numberOfHidingSpots);
+            hidingSpots[randomSpot] = "Bolt";
+
+            Console.WriteLine("The bolt has been hidden.");
+            Console.WriteLine("Searching...");
+
+            for (int i = 0; i < numberOfHidingSpots; i++)
+            {
+                if (hidingSpots[i] == "Bolt")
+                {
+                    Console.WriteLine($"Location found. It's hidden at index {i}.");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
