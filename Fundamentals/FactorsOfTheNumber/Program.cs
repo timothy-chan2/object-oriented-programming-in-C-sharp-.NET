4f4468a1d7bd353611be5ca0cf9d75d94108f730
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-04-25 02:05:15 -0400

    Create a program that repeatedly ask the user to enter an integer number if their input is invalid

---
+++ Fundamentals/FactorsOfTheNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FactorsOfTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer: ");
+            string userInput = Console.ReadLine();
+
+            while (true)
+            {
+                if (int.TryParse(userInput, out int input))
+                {
+                    Console.WriteLine($"The factors of {input} are:");
+
+                    
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. You must enter an integer number.");
+                    Console.Write("Enter an integer: ");
+                    userInput = Console.ReadLine();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
