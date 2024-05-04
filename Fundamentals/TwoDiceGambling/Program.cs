06aae8b4ab8028b42dd773d675aaca42fd920433
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-05-04 00:30:16 -0400

    Create a program to find out after how many rolls of a pair of dice before running out of money

---
+++ Fundamentals/TwoDiceGambling/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace TwoDiceGambling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+            
+                Console.Write("How many dollars do you want to bet? ");
+                string userBalanceString = Console.ReadLine();
+
+                if (int.TryParse(userBalanceString, out int userBalance))
+                {
+                    if (userBalance > 0)
+                    {
+                        Random rng = new Random();
+                        int rollCounter = 0;
+                        int diceOneRoll, diceTwoRoll;
+
+                        while (userBalance > 0)
+                        {
+                            diceOneRoll = rng.Next(1, 7);
+                            diceTwoRoll = rng.Next(1, 7);
+                            rollCounter++;
+
+                            if (diceOneRoll + diceTwoRoll == 7)
+                            {
+                                userBalance += 4;
+                            }
+                            else
+                            {
+                                userBalance--;
+                            }
+                        }
+
+                        Console.WriteLine($"Your money was depleted after {rollCounter} rolls.");
+
+                        Console.ReadLine();
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input!");
+                        Console.WriteLine("You must enter a positive integer.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                    Console.WriteLine("You must enter a positive integer.");
+                }
+            }
+        }
+    }
+}
