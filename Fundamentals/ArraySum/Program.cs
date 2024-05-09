eb78f032daa080e1ba847282e2e13891e6664608
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-05-09 17:46:19 -0400

    Create a program to calculate the sum of a user defined array

---
+++ Fundamentals/ArraySum/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter the length of the array: ");
+                string arrayLengthString = Console.ReadLine();
+
+                if (int.TryParse(arrayLengthString, out int arrayLength) && arrayLength > 0)
+                {
+                    int arrayIndex = 0;
+                    int[] userArray = new int[arrayLength];
+
+                    Console.WriteLine();
+
+                    while (true)
+                    {
+                        Console.Write($"Enter the integer at index {arrayIndex}: ");
+                        string arrayElementString = Console.ReadLine();
+
+                        if (int.TryParse(arrayElementString, out int arrayElement))
+                        {
+                            userArray[arrayIndex] = arrayElement;
+                            arrayIndex++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input! You must enter an integer.");
+                        }
+
+                        if (arrayIndex > arrayLength - 1)
+                        {
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine();
+                    SumOfArray(userArray);
+
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input! You must enter a positive integer.");
+                }
+            }
+        }
+
+        static void SumOfArray(int[] arrayOfInts)
+        {
+            int sum = 0;
+            
+            for (int i = 0; i < arrayOfInts.Length; i++)
+            {
+                sum += arrayOfInts[i];
+            }
+
+            Console.WriteLine($"The sum of the array is {sum}.");
+        }
+    }
+}
