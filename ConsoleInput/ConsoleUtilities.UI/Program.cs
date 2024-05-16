3a9cd525576b82cfe5fd0406f48d879d3ee6fb2f
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-05-16 00:27:25 -0400

    Create a program to use the ConsoleUtilities.BLL

---
+++ ConsoleInput/ConsoleUtilities.UI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ConsoleUtilities.BLL;
+
+namespace ConsoleUtilities.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = UserInput.GetIntFromUser("Enter your age: ");
+
+            Console.WriteLine($"You are {age} years old.");
+            Console.ReadLine();
+        }
+    }
+}
