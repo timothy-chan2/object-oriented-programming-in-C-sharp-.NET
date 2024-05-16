46179b1ebc6b7b368dc4f1f668e943c6106319f4
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-05-15 23:49:07 -0400

    Create UserInput business logic layer

---
+++ ConsoleInput/ConsoleUtilities.BLL/UserInput.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUtilities.BLL
+{
+    public class UserInput
+    {
+        public static int GetIntFromUser(string prompt)
+        {
+            bool isFirstAttempt = true;
+            string userInputString;
+            int IntUserInput;
+
+            do
+            {
+                if (isFirstAttempt)
+                {
+                    isFirstAttempt = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+                Console.Write(prompt);
+                userInputString = Console.ReadLine();
+            } while (!int.TryParse(userInputString, out IntUserInput));
+
+            return IntUserInput;
+        }
+    }
+}
