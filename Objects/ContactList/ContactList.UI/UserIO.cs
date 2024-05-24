a10113b7a01c486db88a7f804ba81670d83336d8
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-05-24 15:48:16 -0400

    Add mothods to handle invalid user inputs

---
+++ Objects/ContactList/ContactList.UI/UserIO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactList.UI
+{
+    public class UserIO
+    {
+        public string ReadString(string prompt)
+        {
+            string userInput = "";
+
+            while (userInput == "")
+            {
+                Console.WriteLine(prompt);
+                userInput = Console.ReadLine().Trim();
+
+                if (userInput == "")
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+
+            return userInput;
+        }
+
+        public int ReadInt(string prompt, int min, int max)
+        {
+            int output;
+
+            while (true)
+            {
+                Console.WriteLine(prompt);
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out output))
+                {
+                    if (output >= min && output <= max)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The number is outside of the range {min} to {max}.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input! Please enter an integer.");
+                }
+            }
+            return output;
+        }
+    }
+}
