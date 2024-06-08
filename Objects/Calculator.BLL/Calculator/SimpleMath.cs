2e2638a2f397a447b6e3ad5c07e29eb51fd3cfd3
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-06-08 18:57:57 -0400

    Create a SimpleMath class to learn about NUnit unity testing framework

---
+++ Objects/Calculator.BLL/Calculator/SimpleMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class SimpleMath
+    {
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
