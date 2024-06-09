4df3b659316bb846a6a960f03df2536138a360ac
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-06-08 20:15:47 -0400

    Create test cases

---
+++ Objects/Calculator.BLL/Calculator.Tests/SimpleMathTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class SimpleMathTests
+    {
+        [Test]
+        public void IntegerDivisionSingleTest()
+        {
+            SimpleMath math = new SimpleMath();
+            int result = math.Divide(5, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [TestCase(4, 2, 2)]
+        [TestCase(13, 6, 2)]
+        [TestCase(-20, 5, -4)]
+        public void IntegerDivisionUsingTestCases(int x, int y, int expected)
+        {
+            SimpleMath math = new SimpleMath();
+            int actual = math.Divide(x, y);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void DivideByZeroTest()
+        {
+            SimpleMath math = new SimpleMath();
+            Assert.Throws<DivideByZeroException>(() => math.Divide(10, 0));
+        }
+    }
+}
