d4f7496fa781ab7d216cef030a6775617a91a82c
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-06-15 23:43:41 -0400

    Create the business logic layer

--- Objects/GuessingGame/GuessingGame.BLL/GameManager.cs
+++ Objects/GuessingGame/GuessingGame.BLL/GameManager.cs
@@ -8,6 +8,55 @@ namespace GuessingGame.BLL
 {
     public class GameManager
     {
-        
+        public const int MinGuess = 1;
+        public const int MaxGuess = 20;
+
+        private int _answer;
+
+        private bool IsValidGuess(int guess)
+        {
+            bool isValid = false;
+
+            if (guess >= MinGuess && guess <= MaxGuess)
+            {
+                isValid = true;
+            }
+
+            return isValid;
+        }
+
+        private void CreateRandomAnswer()
+        {
+            Random rng = new Random();
+            _answer = rng.Next(MinGuess, MaxGuess + 1);
+        }
+
+        public GuessResult ProcessGuess(int guess)
+        {
+            GuessResult guessResult = GuessResult.Invalid;
+
+            if (IsValidGuess(guess))
+            {
+                if (guess < _answer)
+                {
+                    guessResult = GuessResult.Higher;
+                }
+                else if (guess > _answer)
+                {
+                    guessResult = GuessResult.Lower;
+                }
+                else
+                {
+                    guessResult = GuessResult.Equal;
+                }
+            }
+
+            return guessResult;
+        }
+
+        public void Start()
+        {
+            CreateRandomAnswer();
+        }
     }
 }
