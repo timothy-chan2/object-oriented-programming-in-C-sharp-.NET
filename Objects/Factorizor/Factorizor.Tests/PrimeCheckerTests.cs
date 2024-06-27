34fd960703dace882040c2e347993faaeef812a6
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-06-27 02:20:18 -0400

    Create tests for the PrimeChecker class

--- Objects/Factorizor/Factorizor.Tests/PrimeCheckerTests.cs
+++ Objects/Factorizor/Factorizor.Tests/PrimeCheckerTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using Factorizor.BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,35 @@ using System.Threading.Tasks;
 
 namespace Factorizor.Tests
 {
+    [TestFixture]
     class PrimeCheckerTests
     {
+        [Test]
+        public void NotPrimeNumberTest()
+        {
+            FactorFinder factorFinderInstance = new FactorFinder();
+            PrimeChecker primeCheckerInstance = new PrimeChecker();
+            int userSelectedNumber = 12;
 
+            factorFinderInstance.ProcessInput(userSelectedNumber);
+            int numberOfFactors = factorFinderInstance.numberOfFactors;
+
+            bool actual = primeCheckerInstance.IsPrimeNumber(userSelectedNumber, numberOfFactors);
+            Assert.That(actual, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void PrimeNumberTest()
+        {
+            FactorFinder factorFinderInstance = new FactorFinder();
+            PrimeChecker primeCheckerInstance = new PrimeChecker();
+            int userSelectedNumber = 13;
+
+            factorFinderInstance.ProcessInput(userSelectedNumber);
+            int numberOfFactors = factorFinderInstance.numberOfFactors;
+
+            bool actual = primeCheckerInstance.IsPrimeNumber(userSelectedNumber, numberOfFactors);
+            Assert.That(actual, Is.EqualTo(true));
+        }
     }
 }
