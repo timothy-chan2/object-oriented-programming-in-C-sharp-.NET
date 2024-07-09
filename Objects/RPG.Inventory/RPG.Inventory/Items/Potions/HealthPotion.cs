0adc15f33dc82d93f3002ba6e6895b559cf9fcce
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-07-09 00:20:32 -0400

    Create base and derived classes

---
+++ Objects/RPG.Inventory/RPG.Inventory/Items/Potions/HealthPotion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Inventory.Items.Potions
+{
+    public class HealthPotion : Item
+    {
+        public HealthPotion()
+        {
+            Name = "A health potion";
+            Description = "Useful for healing";
+            Weight = 1;
+            Type = "Potion";
+        }
+    }
+}
---
+++ Objects/RPG.Inventory/RPG.Inventory/Items/Weapons/Sword.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Inventory.Items.Weapons
+{
+    public class Sword : Item
+    {
+        public Sword()
+        {
+            Name = "A wooden sword";
+            Description = "Useful for defending yourself";
+            Weight = 3;
+            Type = "Weapon";
+        }
+    }
+}
