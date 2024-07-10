eb1a82994825c530eb9f42297934a3bdaa5b323e
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-07-09 23:11:58 -0400

    Create a class for a type of armor

---
+++ Objects/RPG.Inventory/RPG.Inventory/Items/Armor/MiniShield.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Inventory.Items.Armor
+{
+    public class MiniShield : Item
+    {
+        public MiniShield()
+        {
+            Name = "A small shield";
+            Description = "Useful for protecting yourself";
+            Weight = 3;
+            Type = "Armor";
+        }
+    }
+}
