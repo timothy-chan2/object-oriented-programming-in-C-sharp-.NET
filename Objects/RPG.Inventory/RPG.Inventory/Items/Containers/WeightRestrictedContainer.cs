f6f20a057d021e9cb3c258bf7ee43261166725b7
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-07-14 19:25:57 -0400

    Create a weight restricted container that controls what items can be added based on total weight

---
+++ Objects/RPG.Inventory/RPG.Inventory/Items/Containers/WeightRestrictedContainer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Inventory.Items.Containers
+{
+    public class WeightRestrictedContainer : Container
+    {
+        private int _maxWeight;
+        private int _currentWeight;
+        
+        public WeightRestrictedContainer(int capacity, int maxWeight) : base(capacity)
+        {
+            _maxWeight = maxWeight;
+        }
+
+        public override bool AddItem(Item itemToAdd)
+        {
+            if (_currentWeight + itemToAdd.Weight > _maxWeight)
+            {
+                return false;
+            }
+            
+            bool itemAdded = base.AddItem(itemToAdd);
+
+            if (itemAdded)
+            {
+                _currentWeight += itemToAdd.Weight;
+            }
+
+            return itemAdded;
+        }
+    }
+}
