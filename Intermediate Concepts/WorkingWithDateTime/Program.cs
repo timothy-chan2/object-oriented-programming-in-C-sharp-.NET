f3444ddfcee8574a0cf9cbc8c632ae716aab6482
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-08-06 01:49:11 -0400

    Use the DateTime constructor to create a DateTime object

---
+++ Intermediate Concepts/WorkingWithDateTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mark the variable declaration as nullable if you might need to set it as null
+            DateTime? nullableDate = null;
+
+            //August 6th, 2024 11:45:10 AM
+            nullableDate = new DateTime(2024, 8, 6, 11, 45, 10);
+        }
+    }
+}
