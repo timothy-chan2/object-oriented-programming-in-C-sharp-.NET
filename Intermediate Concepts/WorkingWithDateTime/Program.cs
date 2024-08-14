766361b642859b95fbfc5555f04347d4b8480a6d
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-08-13 23:44:20 -0400

    Add ways to get the current date with DateTime

--- Intermediate Concepts/WorkingWithDateTime/Program.cs
+++ Intermediate Concepts/WorkingWithDateTime/Program.cs
@@ -13,13 +13,23 @@ namespace WorkingWithDateTime
             // Mark the variable declaration as nullable if you might need to set it as null
             DateTime? nullableDate = null;
 
-            //August 6th, 2024 11:45:10 AM
+            // August 6th, 2024 11:45:10 AM
             nullableDate = new DateTime(2024, 8, 6, 11, 45, 10);
 
             Console.Write("Enter a date: ");
             string input = Console.ReadLine();
 
             nullableDate = DateTime.TryParse(input, out DateTime parseDate) ? parseDate : (DateTime?)null;
+
+            // Current date
+            // With time to the millisecond
+            Console.WriteLine(DateTime.Now);
+
+            // With time as 12:00:00 AM
+            Console.WriteLine(DateTime.Today);
+
+            // With time to the millisecond in UTC
+            Console.WriteLine(DateTime.UtcNow);
         }
     }
 }
